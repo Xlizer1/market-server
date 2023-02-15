a1be818a03686a445f2719f76f6e687f3e02e9a6
xlizer1 <mustafa_142@mail.ru>
2023-02-15 21:43:42 +0300

    َ

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Express } from "express";
+import express from "express";
 import config from "config";
 import connect from "./utils/connect";
 import logger from "./utils/logger";
